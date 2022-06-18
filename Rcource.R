5bbad56e8a4c9a182e11ca0f80e24f30efb15690
Juliana Leshchenko <68852795+JuliianaLeshchenko@users.noreply.github.com>
2022-06-18 16:41:24 +0200

    Update Rcource.R

--- Rcource.R
+++ Rcource.R
@@ -153,3 +153,7 @@ p <- ggtree(sarco.nj.rooted) + geom_tiplab(size=3) #nj
 msaplot(p, sarco.mafft.ng, offset=3, width=15)
 p <- ggtree(treeRatchet) + geom_tiplab(size=3) #Maximum parsimony
 msaplot(p, sarco.mafft.ng, offset=3, width=1)
+plot.new()
+polygon(x=c(0,1,1,0),y=c(0,0,0.5,0.5), col="#FFDD00")
+polygon(x=c(0,1,1,0),y=c(0.5,0.5,1,1), col="#0057B7")
+text(0.5, 0.25, labels="Слава Україні", cex=2)
